e6c5b43f1c4843ad44662537f73bad913defb610
dima4567254 <dima.xipkov@gmail.com>
2021-05-10 16:54:52 +0300

    версия с img

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,37 @@
-/*
-const mixin1 = document.qurySelector(".products__list");
+ $(function() {
 
-if (mixin1) {
-    mixitup('.products__list', {
-        selectors: {
-            control: 'filter'
-        }
-    })
-}*/
-var mixer = mixitup('.products__content');
+      
+     
+      $('.slider__items').slick({
+          dots: true,
+          arrows: false,
+          fade: true,
+          autoplay: true,
+          autoplaySpead: 1000
+      });
 
-$(function() {
-    var mixer = mixitup('.products__list');
-
-
-    $('.slider__items').slick({
-        dots: true,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpead: 1000
-    });
+      $('.partners').slick({
+          arrows: false,
+          slidesToShow: 5,
+          infinite: true,
+          slidesToScroll: 5
+      });
 
 
-    var mixer = mixitup('.products__content');
 
-});
 
-var mixer = mixitup('.products__list');
+ });
+
+ 
+
+   var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
