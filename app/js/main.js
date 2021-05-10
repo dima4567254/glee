c2eed2fa335d9a6a5703cc8911ef1d617502cdb7
dima4567254 <dima.xipkov@gmail.com>
2021-05-11 01:23:49 +0300

    версия с img не доделал

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,39 @@
  $(function() {
 
-      
-     
-      $('.slider__items').slick({
-          dots: true,
-          arrows: false,
-          fade: true,
-          autoplay: true,
-          autoplaySpead: 1000
-      });
 
-      $('.partners').slick({
-          arrows: false,
-          slidesToShow: 5,
-          infinite: true,
-          slidesToScroll: 5
-      });
+
+     $('.slider__items').slick({
+         dots: true,
+         arrows: false,
+         fade: true,
+         /*
+                  autoplay: true,
+                  
+                  autoplaySpead: 1000*/
+     });
+
+     $('.partners').slick({
+         arrows: false,
+         slidesToShow: 5,
+         infinite: true,
+         slidesToScroll: 5
+     });
 
 
 
 
  });
 
- 
 
-   var containerEl1 = document.querySelector('[data-ref="container-1"]');
-  var containerEl2 = document.querySelector('[data-ref="container-2"]');
- 
-  var config = {
-    controls: {
-      scope: 'local'
-    }
-  };
- 
-  var mixer1 = mixitup(containerEl1, config);
-  var mixer2 = mixitup(containerEl2, config);
+
+ var containerEl1 = document.querySelector('[data-ref="container-1"]');
+ var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+ var config = {
+     controls: {
+         scope: 'local'
+     }
+ };
+
+ var mixer1 = mixitup(containerEl1, config);
+ var mixer2 = mixitup(containerEl2, config);
