9691a277239a71289352b413ad718c1890745da0
dima4567254 <dima.xipkov@gmail.com>
2021-05-19 05:36:53 +0300

    18,04,20

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
  $(function() {
 
-
-
      $('.slider__items').slick({
          dots: true,
          arrows: false,
@@ -20,14 +18,27 @@
      });
 
 
+     $(".prise").ionRangeSlider({
+         type: "double",
+         min: 0,
+         max: 1000,
+         from: 200,
+         to: 500,
+         grid: true
+     });
 
+     $(".star").rateYo({
+         starWidth: "40px",
+         rating: 3.6,
+         normalFill: "#A0A0A0",
+         readOnly: true
+     });
 
  });
 
-
-
  var containerEl1 = document.querySelector('[data-ref="container-1"]');
  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ var containerEl3 = document.querySelector('[data-ref="container-3"]');
 
  var config = {
      controls: {
@@ -37,3 +48,22 @@
 
  var mixer1 = mixitup(containerEl1, config);
  var mixer2 = mixitup(containerEl2, config);
+ var mixer3 = mixitup(containerEl3, config);
+
+
+
+
+
+ /* var containerEl1 = document.querySelector('[data-ref="container-1"]');
+ var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ var containerEl3 = document.querySelector('[data-ref="container-3"]');
+
+ var config = {
+     controls: {
+         scope: 'local'
+     }
+ };
+
+ var mixer1 = mixitup(containerEl1, config);
+ var mixer2 = mixitup(containerEl2, config);
+ var mixer3 = mixitup(containerEl3, config);*/
--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,10 @@ function scripts() {
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
             'node_modules/mixitup/dist/mixitup.js',
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+            'node_modules/rateyo/src/jquery.rateyo.js',
             'app/js/main.js'
+
         ])
         .pipe(concat('main.min.js'))
         .pipe(uglify())
@@ -88,7 +91,8 @@ function cleanDist() {
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/**/*.html']).on('change', browserSync.reload);
+    watch(['app/*.html']).on('change', browserSync.reload);
+
 }
 
 exports.styles = styles;
