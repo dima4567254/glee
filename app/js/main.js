0b294b947b8729efbb32fcf4e77647b1e25c180f
dima4567254 <dima.xipkov@gmail.com>
2021-07-15 22:30:51 +0300

    last2

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,15 @@
 
      });
 
+     $(".menu a").on("click", function(event) {
+         event.preventDefault();
+         var id = $(this).attr('href'),
+             top = $(id).offset().top;
+         $('body,html').animate({
+             scrollTop: top
+         }, 1500);
+     });
+
 
      $('.product-tabs__item').on('click', function(e) {
          e.preventDefault();
